f130c1783a95ce4ceab64b6d4ff17c5701e78107
BriFoxy <143095436+BriFoxy@users.noreply.github.com>
2023-09-16 00:31:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+ const card = document.getElementById('card');
 
+        function dislike() {
+            card.classList.add('disliked');
+            setTimeout(() => {
+                // Entfernen der Karte nach der Animation
+                card.remove();
+            }, 300);
+        }
+
+        function like() {
+            card.classList.add('liked');
+            setTimeout(() => {
+                // Entfernen der Karte nach der Animation
+                card.remove();
+            }, 300);
+        }
